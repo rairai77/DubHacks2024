c719f607002546af951cc9f7d24114f8a01d6599
rairai77 <cutenitki@gmail.com>
2024-10-12 20:40:27 -0700

    fixed counter

--- webserverTest/index.js
+++ webserverTest/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
-const counter = 0;
+let counter = 0;
 app.get("/", (req, res) => {
     counter++;
     res.send("Howdy Neighbor " + counter);
