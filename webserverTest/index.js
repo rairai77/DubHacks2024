884d285c9beaa59f0f7bb0aab3f85fb763758d5a
rairai77 <cutenitki@gmail.com>
2024-10-12 23:41:44 -0700

    debugging decoder

--- webserverTest/index.js
+++ webserverTest/index.js
@@ -24,7 +24,8 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   // Check if req.body.data is base64 encoded, and decode it
-    console.log(req.body);
+    console.log(req);
+  console.log(req.body);
   if (req.body.data) {
     const decodedBuffer = base64ToArrayBuffer(req.body.data); // Convert base64 to ArrayBuffer
     frames.push(decodedBuffer);
